f80bf303f8cc606dbc5c1da22198c1c81a8a0a13
Glen Wamaru <garpirungu@gmail.com>
2025-04-18 18:44:08 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, nameToMatch) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === nameToMatch.toLowerCase();
+    });
+}
+
+
+function fuzzyMatch(drivers, nameToMatch) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().startsWith(nameToMatch.toLowerCase());
+    })
+}
+
+function matchName(drivers, nameToMatch) {
+    return drivers.filter(function(driver) {
+        return driver.name === nameToMatch;
+    });
+}
